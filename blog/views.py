f2db8c657dbe22e82ac16e60d1c5af527424e64d
abdumannopov802 <154593414+abdumannopov802@users.noreply.github.com>
2024-01-23 01:28:02 -0800

    initial commit

--- blog/views.py
+++ blog/views.py
@@ -2,30 +2,21 @@ from django.http import HttpResponse
 
 def sayhello(request):
     return HttpResponse('Hello workd...')
-
 def mainview(request):
     return HttpResponse ("Main View")
-
 def uzbekistan(request):
     return HttpResponse ("O'zbekiston yangiliklari bo'limi")
-
 def jahon(request):
     return HttpResponse ("Jahon Yangiliklari bo'limi")
-
 def iqtisodiyot(request):
     return HttpResponse ("Iqtisodiyot yangiliklri bo'limi")
-
 def jamiyat(request):
     return HttpResponse ("Jamiyat bo'limi")
-
 def fan_texnika(request):
     return HttpResponse ("Fan va Texnika yangiliklari")
-
 def sport(request):
     return HttpResponse ("Sport yangiliklari")
-
 def nuqtainazar(request):
     return HttpResponse ("Nuqtai Nazar")
-
 def audio(request):
     return HttpResponse ("Audio bo'limi")
